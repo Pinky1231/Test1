20f4c6a0d13509c7815337bb44f652690c23d81f
Pinky <tst@mail.ru>
2026-05-04 16:49:30 +0300

    patched hash version with removed bags TEST

--- main.go
+++ main.go
@@ -1,53 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
 )
 
+type Item struct {
+	value interface{}
+	ttl   time.Time
+}
+
 type Cash struct {
-	data map[string]interface{}
+	data map[string]Item
 	mx   sync.RWMutex
 	wg   sync.WaitGroup
 }
 
 func main() {
 	Csh := Cash{
-		data: make(map[string]interface{}),
+		data: make(map[string]Item),
 	}
+	deadline := time.Now().Add(time.Second * 16)
+	context, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
 
-	// Csh.Set("Rok", "Huynya", time.Second*6)
-	// Csh.Set("Bob", 21, time.Second*3)
-	// Csh.Set("Ric_astley", 67, time.Minute*15)
-	// Csh.Set("John", 14, time.Minute*1)
+	Csh.wg.Add(1)
+	go Csh.cleaner(context)
 
-	// fmt.Println(Csh.data)
-	// fmt.Println(Csh.Get("Rick_astley"))
-	// <-time.After(time.Second * 7)
-	// fmt.Println(Csh.data)
+	Csh.Set("Rok", "Huynya", time.Second*6)
+	Csh.Set("Bob", 21, time.Second*3)
+	Csh.Set("Ric_astley", 67, time.Second*15)
+	Csh.Set("John", 14, time.Minute*1)
+
+	fmt.Println(Csh.data)
+	<-time.After(time.Second * 7)
+	fmt.Println(Csh.Get("Bob"))
+	fmt.Println(Csh.data)
 
 	Csh.wg.Wait()
 
 }
 
 func (cash *Cash) Set(key string, value interface{}, ttl time.Duration) {
-	if _, ok := cash.Get(key); !ok {
-		cash.mx.Lock()
-		defer cash.mx.Unlock()
-		cash.data[key] = value
-		cash.wg.Add(1)
-		go func(ttl time.Duration) {
-			defer cash.wg.Done()
+	cash.mx.Lock()
+	defer cash.mx.Unlock()
 
-			<-time.After(ttl)
-
-			cash.mx.Lock()
-			defer cash.mx.Unlock()
-			fmt.Println(key, "был удален спустя", ttl)
-			delete(cash.data, key)
-
-		}(ttl)
+	cash.data[key] = Item{
+		value,
+		time.Now().Add(ttl).Local(),
 	}
 
 }
@@ -55,12 +57,43 @@ func (cash *Cash) Set(key string, value interface{}, ttl time.Duration) {
 func (cash *Cash) Get(key string) (interface{}, bool) {
 	cash.mx.RLock()
 	defer cash.mx.RUnlock()
+	val, ok := cash.data[key]
 
-	result, ok := cash.data[key]
-	if !ok {
+	if !ok { //if key does'nt exits
 		return nil, false
-	} else {
-		return result, true
+	} else if time.Now().UnixNano() > val.ttl.UnixNano() { // if time is expired
+		return nil, false
+	}
+	return val.value, ok
+
+}
+
+func (cash *Cash) cleaner(ctx context.Context) {
+	defer cash.wg.Done()
+	ticker := time.NewTicker(time.Second)
+
+	for {
+		select {
+
+		case <-ctx.Done():
+			ticker.Stop()
+			fmt.Println("Монитроинг завершен")
+			return
+
+		case <-ticker.C:
+			cash.mx.Lock()
+
+			for key, obj := range cash.data {
+				if time.Now().UnixNano() > obj.ttl.UnixNano() {
+
+					fmt.Println(key, "был удален")
+					delete(cash.data, key)
+
+				}
+
+			}
+			cash.mx.Unlock()
+		}
 	}
 
 }
